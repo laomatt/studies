1e4b91ef353107dc4ca35ad61858f488d2602135
laomatt <laomatt1@gmail.com>
2016-03-10 21:38:27 -0800

    require tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require quick_studies
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
 //= require_tree .
