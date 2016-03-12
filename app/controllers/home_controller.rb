59f76b1dbd11e78d92590ad653f9a8d8ebe85a75
laomatt <laomatt1@gmail.com>
2016-03-12 04:52:41 -0800

    delete thumbnail too

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -85,6 +85,7 @@ class HomeController < ApplicationController
     slide = Slide.create(:ext_url => obj.public_url.to_s, :slideshow_id => @slideshow.id, :title => uploaded_io.original_filename, :on_s3 => true, :thumb_url => obj_thumb.public_url.to_s)
 
     File.delete(Rails.root + "tmp/#{uploaded_io.original_filename}")
+    File.delete(Rails.root + "tmp/thumbnail-#{uploaded_io.original_filename}")
     render :json => slide
   end
 
