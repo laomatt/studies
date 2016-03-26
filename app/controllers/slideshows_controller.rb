a927695f1db9abd00a177ba1418962654edc97d0
laomatt <laomatt1@gmail.com>
2016-03-26 14:25:44 -0700

    header image change

--- app/controllers/slideshows_controller.rb
+++ app/controllers/slideshows_controller.rb
@@ -12,8 +12,12 @@ class SlideshowsController < ApplicationController
 
   def edit
     @slideshow = Slideshow.find(params[:id])
-    @images = @slideshow.slides.order(:position)
-    @user_ps = @slideshow.slide_show_permissions.map { |e| e.user }.uniq
+    if @slideshow.user_id == current_user.id
+      @images = @slideshow.slides.order(:position)
+      @user_ps = @slideshow.slide_show_permissions.map { |e| e.user }.uniq
+    else
+      redirect_to '/'
+    end
   end
 
   def draw_set
