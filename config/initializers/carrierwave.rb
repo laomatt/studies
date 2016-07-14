c3fadf0bb798011f253f8e114488743237754517
laomatt <laomatt1@gmail.com>
2016-07-13 23:37:27 -0700

    config vars

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,10 +15,10 @@
     :region                => ENV['S3_REGION']
 }
 
-config.storage = :fog
+  config.storage = :fog
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
   config.fog_directory    = ENV['S3_BUCKET']
-  # config.s3_access_policy = :public_read
+  config.s3_access_policy = :public_read
   # config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET']}"
 end
