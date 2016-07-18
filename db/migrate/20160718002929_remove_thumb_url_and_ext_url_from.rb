5e26e1f0054ad28c7dbd27fee2e5742b9998d9e0
laomatt <laomatt1@gmail.com>
2016-07-17 23:31:39 -0700

    save

--- db/migrate/20160718002929_remove_thumb_url_and_ext_url_from.rb
+++ db/migrate/20160718002929_remove_thumb_url_and_ext_url_from.rb
@@ -1,6 +1,10 @@
 class RemoveThumbUrlAndExtUrlFrom < ActiveRecord::Migration
   def change
-    rename_column :slides, :thumb_url, :first_thumb_url
-    rename_column :slides, :exl_url, :first_ext_url
+   change_table :slides do |t|
+      t.rename :ext_url, :first_ext_url
+      t.rename :thumb_url, :first_thumb_url
+    end
+    # rename_column :slides, :exl_url, :first_ext_url
+    # rename_column :slides, :thumb_url, :first_thumb_url
   end
 end
