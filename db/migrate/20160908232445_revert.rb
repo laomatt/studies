ef4edcf901cb81dddd7838b1c13c5e96248aa249
laomatt <laomatt1@gmail.com>
2016-09-08 16:40:19 -0700

    remove migratino

--- db/migrate/20160908232445_revert.rb
+++
@@ -1,8 +0,0 @@
-class Revert < ActiveRecord::Migration
-  def change
-   change_table :slides do |t|
-      t.rename :first_ext_url, :ext_url
-      t.rename :first_thumb_url, :thumb_url
-    end
-  end
-end
